af3062d43c9ccdd3b3f6590eb8b596389228f7aa
Ash5771 <22f3000982@ds.study.iitm.ac.in>
2025-05-07 13:43:09 +0530

    update

--- backup.sql
+++ backup.sql
@@ -6,8 +6,6 @@ CREATE TABLE assignments (
             yt_link TEXT, watch_count INTEGER DEFAULT 0,
             FOREIGN KEY (course_id) REFERENCES courses(id)
         );
-INSERT INTO "assignments" VALUES(1,1,'new c','https://youtu.be/VT9ToOPawW0',2);
-INSERT INTO "assignments" VALUES(2,1,'sampledd','https://youtu.be/tnInGENQaVc',0);
 INSERT INTO "assignments" VALUES(3,3,'MLF  End Term PYQ  | Jan 23 FN','https://youtu.be/4uUYBwIQMNg',0);
 INSERT INTO "assignments" VALUES(4,3,'MLF  End Term PYQ  | May 23 FN','https://youtu.be/Bv4NA-TDdro',0);
 INSERT INTO "assignments" VALUES(5,3,'MLF  End Term PYQ  | May 23 AN','https://youtu.be/oFShvOKqjxg',1);
@@ -45,7 +43,6 @@ CREATE TABLE notes (
             yt_link TEXT, watch_count INTEGER DEFAULT 0,
             FOREIGN KEY (course_id) REFERENCES courses(id)
         );
-INSERT INTO "notes" VALUES(2,1,'sample c','https://youtu.be/1ecukKO2r5g',2);
 INSERT INTO "notes" VALUES(3,3,'MLF Quiz-2 PYQ  | Jan 2023','https://youtu.be/t0zDodn69fk',0);
 INSERT INTO "notes" VALUES(4,3,'MLF Quiz-2 PYQ  | May 2023','https://youtu.be/1aFlukVN9vM',0);
 INSERT INTO "notes" VALUES(5,3,'MLF Quiz-2 PYQ  | September 2023','https://youtu.be/vaj4TkwDGCI',0);
@@ -100,7 +97,7 @@ INSERT INTO "pyqs" VALUES(27,5,'PDSA Week-3 PYQ','https://youtu.be/4iNU_E2_y9k',
 INSERT INTO "pyqs" VALUES(28,5,'PDSA Week-4 PYQ','https://youtu.be/V2RZ8PY37Ts',0);
 INSERT INTO "pyqs" VALUES(29,5,'PDSA Quiz-1 PYQ | Jan 24','https://youtu.be/FNfmEJqK8bU',0);
 INSERT INTO "pyqs" VALUES(30,5,'PDSA Quiz-1 PYQ | May 24','https://youtu.be/MGchbfQqXt4',0);
-INSERT INTO "pyqs" VALUES(32,9,'DBMS OPPE PYQ PlayList','https://www.youtube.com/playlist?list=PLTewhG5vbW7EKmyl32LAzSJLcEtuK1fer',0);
+INSERT INTO "pyqs" VALUES(32,9,'DBMS OPPE PYQ PlayList','https://www.youtube.com/playlist?list=PLTewhG5vbW7EKmyl32LAzSJLcEtuK1fer',1);
 INSERT INTO "pyqs" VALUES(33,9,'DBMS OPPE PYQ PlayList ( Developer Harsh)','https://youtube.com/playlist?list=PLDfna1ApN44qNuRotBr2ahcs9Khvesw3O&si=KPmpMeTun4dN1-1L',0);
 DELETE FROM "sqlite_sequence";
 INSERT INTO "sqlite_sequence" VALUES('courses',12);
